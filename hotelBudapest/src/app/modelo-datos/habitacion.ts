87b56297dde1e6b0539a2b3a8112d5512e79d3f5
manana <manana>
2016-12-20 14:00:47 +0100

    working

---
+++ hotelBudapest/src/app/modelo-datos/habitacion.ts
@@ -0,0 +1,11 @@
+export class Habitacion {
+
+    private _id: String;
+    private tipo: String;
+    private categoria: String;
+    private fechaEnt: any;
+    private fechaSal: any;
+
+    constructor (){}
+
+}
